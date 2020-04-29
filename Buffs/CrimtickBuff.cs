d98ad480fc169d45d065d0ca7adad2dabc9afb28
termigator1359 <45545085+termigator1359@users.noreply.github.com>
2020-04-29 17:19:48 -0400

    buffs now use modcontent

--- Buffs/CrimtickBuff.cs
+++ Buffs/CrimtickBuff.cs
@@ -3,24 +3,29 @@ using Terraria;
 using Terraria.ModLoader;
 using Terraria.ID;
 using Microsoft.Xna.Framework;
+using CelestialInfernalMod.Projectiles.Minions;
 using static Terraria.ModLoader.ModContent;
 
 namespace CelestialInfernalMod.Buffs
 {
     public class CrimtickBuff : ModBuff
 	{
-		public override void SetDefaults() {
+		public override void SetDefaults()
+		{
 			DisplayName.SetDefault("Crimtick");
 			Description.SetDefault("The only thing it spreads is pain");
 			Main.buffNoSave[Type] = true;
 			Main.buffNoTimeDisplay[Type] = true;
 		}
 
-		public override void Update(Player player, ref int buffIndex) {
-			if (player.ownedProjectileCounts[mod.ProjectileType("Crimtick")] > 0) {
+		public override void Update(Player player, ref int buffIndex)
+		{
+			if (player.ownedProjectileCounts[ProjectileType<Crimtick>()] > 0)
+			{
 				player.buffTime[buffIndex] = 18000;
 			}
-			else {
+			else
+			{
 				player.DelBuff(buffIndex);
 				buffIndex--;
 			}
--- Buffs/EaterBuff.cs
+++ Buffs/EaterBuff.cs
@@ -3,24 +3,29 @@ using Terraria;
 using Terraria.ModLoader;
 using Terraria.ID;
 using Microsoft.Xna.Framework;
+using CelestialInfernalMod.Projectiles.Minions;
 using static Terraria.ModLoader.ModContent;
 
 namespace CelestialInfernalMod.Buffs
 {
     public class EaterBuff : ModBuff
 	{
-		public override void SetDefaults() {
+		public override void SetDefaults()
+		{
 			DisplayName.SetDefault("Leather-Skinned Beast");
 			Description.SetDefault("It eats souls for you");
 			Main.buffNoSave[Type] = true;
 			Main.buffNoTimeDisplay[Type] = true;
 		}
 
-		public override void Update(Player player, ref int buffIndex) {
-			if (player.ownedProjectileCounts[mod.ProjectileType("LeatherEater")] > 0) {
+		public override void Update(Player player, ref int buffIndex)
+		{
+			if (player.ownedProjectileCounts[ProjectileType<LeatherEater>()] > 0)
+			{
 				player.buffTime[buffIndex] = 18000;
 			}
-			else {
+			else
+			{
 				player.DelBuff(buffIndex);
 				buffIndex--;
 			}
