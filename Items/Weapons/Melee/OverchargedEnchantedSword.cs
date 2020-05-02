95cdd1d491df04eedf7c3c5d08cfe3d4f927dece
Andrzinho15 <59664190+Andrzinho15@users.noreply.github.com>
2020-05-02 18:03:57 -0300

    Adding updated Overcharged Enchanted Sword

---
+++ Items/Weapons/Melee/OverchargedEnchantedSword.cs
@@ -0,0 +1,63 @@
+using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace CelestialInfernalMod.Items.Weapons.Melee.Hardmode
+{
+	public class OverchargedEnchantedSword : ModItem
+	{
+		public override void SetStaticDefaults() 
+		{
+            DisplayName.SetDefault("Overcharged Enchanted Sword");
+			Tooltip.SetDefault("Fires crystals when swung");
+		}
+
+		public override void SetDefaults() 
+		{
+			item.damage = 65;
+			item.melee = true;
+			item.width = 36;
+			item.height = 36;
+			item.useTime = 18;
+			item.useAnimation = 18;
+			item.useStyle = 1;
+			item.knockBack = 8;
+			item.value = Item.buyPrice(0, 18, 0, 0);
+			item.rare = 5;
+			item.UseSound = SoundID.Item1;
+			item.autoReuse = true;
+			item.shoot = ProjectileID.EnchantedBeam;
+			item.shootSpeed = 6f;
+		}
+
+		public override bool Shoot(Player player, ref Vector2 position, ref float speedX, ref float speedY, ref int type, ref int damage, ref float knockBack)
+		{
+			float numberProjectiles = 3; // 3, 4, or 5 shots
+			float rotation = MathHelper.ToRadians(10);
+			position += Vector2.Normalize(new Vector2(speedX, speedY)) * 2f;
+			for (int i = 0; i < numberProjectiles; i++)
+			{
+				Vector2 perturbedSpeed = new Vector2(speedX, speedY).RotatedBy(MathHelper.Lerp(-rotation, rotation, i / (numberProjectiles - 1))) * 2f; // Watch out for dividing by 0 if there is only 1 projectile.
+				Projectile.NewProjectile(position.X, position.Y, perturbedSpeed.X, perturbedSpeed.Y, type, damage, knockBack, player.whoAmI);
+			}
+			return false;
+		}
+
+			public override void AddRecipes()
+		{
+			ModRecipe recipe = new ModRecipe(mod);
+			recipe.AddIngredient(ItemID.EnchantedSword);
+			recipe.AddIngredient(ItemID.LightShard, 10);
+			recipe.AddIngredient(ItemID.DarkShard, 10);
+			recipe.AddTile(TileID.MythrilAnvil);
+			recipe.SetResult(this);
+			recipe.AddRecipe();
+		}
+	}
+}
